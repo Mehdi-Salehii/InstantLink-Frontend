85370e31689bb9fc27f994ed7e0206c762280ed1
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-09-26 00:28:58 +0330

    added message bubble component

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,29 +1,39 @@
 import { SendHorizontalIcon } from "lucide-react";
 import { Input } from "./ui/input";
 import { FormEvent, useRef, useState } from "react";
+import MessageBubble from "./MessageBubble";
+
 export default function ChatBox() {
-  const [message, setMessage] = useState("");
+  const [curMessage, setCurMessage] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
   const msgBoxRef = useRef<null | HTMLDivElement>(null);
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    if (msgBoxRef.current) {
-      msgBoxRef.current.innerHTML += `<div>${message}</div>`;
-      setMessage("");
+    if (curMessage.trim()) {
+      setMessages((pervMessages) => [...pervMessages, curMessage]);
+      setCurMessage("");
     }
   };
   return (
     <div className="flex flex-col grow">
       <div ref={msgBoxRef} className="grow bg-yellow-400 sm:p-3 p-2">
-        message box
+        {messages.map((message, i) => (
+          <MessageBubble
+            isSent={true}
+            key={i}
+            message={message}
+            timestamp={Date.now()}
+          />
+        ))}
       </div>
       <form
         className=" bg-blue-400 sm:p-3 p-2 flex gap-2"
         onSubmit={handleSubmit}
       >
         <Input
-          placeholder="Message"
-          value={message}
-          onChange={(e) => setMessage(e.currentTarget.value)}
+          placeholder="curMessage"
+          value={curMessage}
+          onChange={(e) => setCurMessage(e.currentTarget.value)}
         />
         <button className=" rounded-full border bg-blue-950 border-black/40  flex aspect-square w-10 h-10 justify-center items-center">
           <SendHorizontalIcon className="text-white " />
---
+++ src/components/MessageBubble.tsx
@@ -0,0 +1,33 @@
+const MessageBubble = ({
+  message,
+  isSent,
+  timestamp,
+  key,
+}: {
+  key: number | string;
+  message: string;
+  isSent: boolean;
+  timestamp: number;
+}) => {
+  return (
+    <div
+      key={key}
+      className={`flex ${isSent ? "justify-end" : "justify-start"} mb-2`}
+    >
+      <div
+        className={`relative max-w-xs px-4 py-2 rounded-lg ${isSent ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-900"} ${isSent ? "rounded-br-none" : "rounded-bl-none"}`}
+      >
+        <p className="mb-2">{message}</p>
+        <span
+          className={`text-xs text-${isSent ? "white" : "black"} absolute bottom-[2px] ${isSent ? "left-[3px]" : "right-[3px]"} `}
+        >
+          {new Intl.DateTimeFormat("en-GB", {
+            timeStyle: "short",
+          }).format(timestamp)}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default MessageBubble;
