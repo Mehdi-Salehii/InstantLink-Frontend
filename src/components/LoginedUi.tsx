fa50919368da8deb75255725514792644bad8651
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-09-27 01:15:30 +0330

    minor change

--- src/components/LoginedUi.tsx
+++ src/components/LoginedUi.tsx
@@ -1,12 +1,11 @@
 import ChatBox from "./ChatBox";
 import { ChatsList } from "./ChatsList";
 
-
 export default function LoginedUi() {
   return (
     <div key={"loginedui"} className=" flex flex-col  grow">
-      <ChatsList />
-      {false && <ChatBox />}
+      <ChatBox />
+      {false && <ChatsList />}
     </div>
   );
 }
