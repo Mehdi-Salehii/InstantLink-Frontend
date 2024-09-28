43db7d8358a957c4f2eee8a10cdb0537f4815188
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-09-28 15:17:45 +0330

    modifed firebase initialization method

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -10,13 +10,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA62Qnh3x_7-AdIAAlAeLDWb9yqDfUmmVs",
-  authDomain: "instantlink-b93e2.firebaseapp.com",
-  projectId: "instantlink-b93e2",
-  storageBucket: "instantlink-b93e2.appspot.com",
-  messagingSenderId: "91133061701",
-  appId: "1:91133061701:web:5eb1b6d9f623d8d842d12c",
-  measurementId: "G-2GQFX2GBZV",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
